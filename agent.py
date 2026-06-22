7dd7711beb1c821ae99f9d10e818e4c2dc722f0e
taeyeonha <taeyeonha10@gmail.com>
2026-06-22 01:59:30 -0400

    start with a simpler environment (cart pole)

---
+++ agent.py
@@ -0,0 +1,22 @@
+import os
+os.environ["SDL_AUDIODRIVER"] = "dummy"
+
+import flappy_bird_gymnasium
+import gymnasium
+# env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
+env = gymnasium.make("CartPole-v1", render_mode="human")
+
+obs, _ = env.reset()
+while True:
+    # Next action:
+    # (feed the observation to your agent here)
+    action = env.action_space.sample()
+
+    # Processing:
+    obs, reward, terminated, _, info = env.step(action)
+    
+    # Checking if the player is still alive
+    if terminated:
+        break
+
+env.close()
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from dqn-flappy-bird!")
-
-
-if __name__ == "__main__":
-    main()
